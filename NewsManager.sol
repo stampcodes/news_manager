278c747580b0f5ab750bd3292426e94246e656f9
Andre <andreafragnelli@gmail.com>
2024-07-11 15:20:25 +0200

    aggiunte funzioni di add/remove validators

--- NewsManager.sol
+++ NewsManager.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
 
-
 pragma solidity ^0.8.0;
 
-
 contract NewsManager {
-  
+    address[] public validators;
+
+    function addValidator(address _validator) public {
+        for (uint i = 0; i < validators.length; i++) {
+            require(validators[i] != _validator, "Address already added.");
+        }
+        validators.push(_validator);
+    }
+
+
+
+
+    function removeValidator(address _validator) public {
+        for (uint i = 0; i < validators.length; i++ ){
+            if(_validator == validators[i]){
+                validators[i] = validators[validators.length - 1];
+                validators.pop();
+                return;
+            }
+        }
+        revert("The validator does not exist.");
+    }
+
+
 }
