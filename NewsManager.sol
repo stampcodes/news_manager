8ef74c094d63c1e920a6a6309d4bc42656e592c7
Andre <andreafragnelli@gmail.com>
2024-07-15 13:58:28 +0200

    conclusione progetto

--- NewsManager.sol
+++ NewsManager.sol
@@ -1,30 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.0;
 
+import "./NewsLibrary.sol";
+
 contract NewsManager {
+    using NewsLibrary for address[];
+    using NewsLibrary for NewsLibrary.News;
+
     address public admin;
     address[] public validators;
     mapping(address => uint256) public rewards;
     uint256 public totalRewards;
 
-    struct News {
-        address newsAddress;
-        string name;
-        uint256 deadline;
-        uint256 minValidations;
-        bool validated;
-        uint256 validationCount;
-    }
-
-    News[] public newsList;
+    NewsLibrary.News[] public newsList;
     mapping(uint256 => mapping(address => bool)) public newsValidations;
 
     constructor() {
         admin = msg.sender;
     }
-
-    receive() external payable {}
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can perform this action.");
@@ -32,28 +25,25 @@ contract NewsManager {
     }
 
     function addValidator(address _validator) public onlyAdmin {
-        for (uint256 i = 0; i < validators.length; i++) {
-            require(validators[i] != _validator, "Address already added.");
-        }
+        require(!validators.isValidator(_validator), "Address already added.");
         validators.push(_validator);
     }
 
     function removeValidator(address _validator) public onlyAdmin {
-        for (uint256 i = 0; i < validators.length; i++) {
-            if (_validator == validators[i]) {
+        require(
+            validators.isValidator(_validator),
+            "The validator does not exist."
+        );
+        for (uint i = 0; i < validators.length; i++) {
+            if (validators[i] == _validator) {
                 validators[i] = validators[validators.length - 1];
                 validators.pop();
                 return;
             }
         }
-        revert("The validator does not exist.");
     }
 
-    function assignReward(address _validator, uint256 _amount)
-        public
-        onlyAdmin
-    {
-        require(isValidator(_validator), "Address is not a validator.");
+    function assignReward(address _validator, uint256 _amount) internal {
         require(
             address(this).balance >= _amount,
             "Not enough balance in contract."
@@ -61,15 +51,6 @@ contract NewsManager {
         rewards[_validator] += _amount;
         totalRewards += _amount;
         payable(_validator).transfer(_amount);
-    }
-
-    function isValidator(address _validator) public view returns (bool) {
-        for (uint256 i = 0; i < validators.length; i++) {
-            if (validators[i] == _validator) {
-                return true;
-            }
-        }
-        return false;
     }
 
     function getReward(address _validator) public view returns (uint256) {
@@ -83,7 +64,7 @@ contract NewsManager {
         uint256 _minValidations
     ) public onlyAdmin {
         newsList.push(
-            News({
+            NewsLibrary.News({
                 newsAddress: _newsAddress,
                 name: _name,
                 deadline: _deadline,
@@ -94,30 +75,30 @@ contract NewsManager {
         );
     }
 
-    function getNews(uint256 _index)
+    function getNews(
+        uint _index
+    )
         public
         view
-        returns (
-            address,
-            string memory,
-            uint256,
-            uint256,
-            bool
-        )
+        returns (address, string memory, uint256, uint256, bool, uint256)
     {
-        News storage n = newsList[_index];
+        NewsLibrary.News storage n = newsList[_index];
         return (
             n.newsAddress,
             n.name,
             n.deadline,
             n.minValidations,
-            n.validated
+            n.validated,
+            n.validationCount
         );
     }
 
-    function validateNews(uint256 _newsIndex) public {
-        require(isValidator(msg.sender), "Only validators can validate news.");
-        News storage n = newsList[_newsIndex];
+    function validateNews(uint _newsIndex) public {
+        require(
+            validators.isValidator(msg.sender),
+            "Only validators can validate news."
+        );
+        NewsLibrary.News storage n = newsList[_newsIndex];
         require(block.timestamp <= n.deadline, "Validation period has ended.");
         require(
             !newsValidations[_newsIndex][msg.sender],
@@ -127,15 +108,15 @@ contract NewsManager {
         newsValidations[_newsIndex][msg.sender] = true;
         n.validationCount += 1;
 
-        if (n.validationCount >= n.minValidations) {
+        if (n.isNewsValidated()) {
             n.validated = true;
             assignReward(msg.sender, 1 ether);
         }
     }
 
-    function isNewsValidated(uint256 _newsIndex) public view returns (bool) {
-        News storage n = newsList[_newsIndex];
-        return n.validated;
+    function isNewsValidated(uint _newsIndex) public view returns (bool) {
+        NewsLibrary.News storage n = newsList[_newsIndex];
+        return n.isNewsValidated();
     }
 
     function emergencyWithdraw(address payable _to) public onlyAdmin {
@@ -144,4 +125,12 @@ contract NewsManager {
     }
 
     function addFunds() public payable onlyAdmin {}
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getValidatorCount() public view returns (uint256) {
+        return validators.getValidatorCount();
+    }
 }
